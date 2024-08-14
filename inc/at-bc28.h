e56896c4e10986a058c34c9be9570b19966db5f6
ncm321 <163544372+ncm321@users.noreply.github.com>
2024-08-14 20:25:20 +0800

    Update at-bc28.h

--- inc/at-bc28.h
+++ inc/at-bc28.h
@@ -1,9 +1,15 @@
-/*
- * NB-IoT_test.h
+/*********************************************************************************
+ *      Copyright:  (C) 2024 niuchunmin
+ *                  All rights reserved.
  *
- *  Created on: 2024年7月3日
- *      Author: niuchunmin
- */
+ *       Filename:  at-bc28.h
+ *    Description:  This file 
+ *                 
+ *        Version:  1.0.0(16/07/24)
+ *         Author:  Niu Chunmin <2430815397@qq.com>
+ *      ChangeLog:  1, Release initial version on "16/07/24 20:21:26"
+ *                 
+ ********************************************************************************/
 
 #ifndef INC_AT_BC28_H_
 #define INC_AT_BC28_H_
